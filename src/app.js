9b3f9bd6a076d000b74edb1bd84c726cadba70a1
LauraMarie22 <lepper.laura@googlemail.com>
2025-10-02 13:35:14 +0200

    added current temperature element through API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+function updateTemperature(response) {
+  let temperatureElement = document.querySelector("#weather-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "dd0bbc35t3574da4304bo5c69083ffbb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateTemperature);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Berlin");
